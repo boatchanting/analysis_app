cbddc0a2d54ccf63aa884482cb41fe153f0f4a4e
boatchanting <162798997+boatchanting@users.noreply.github.com>
2024-11-23 23:31:39 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QTabWidget
+from PyQt5.QtGui import QGuiApplication
 from data_loader import DataLoader
 from data_preview import DataPreview
 from data_analysis import DataAnalysis
@@ -8,8 +9,18 @@ class MainApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("数据分析软件")
-        self.setGeometry(100, 100, 800, 600)
+                # 获取屏幕尺寸
+        screen = QGuiApplication.primaryScreen()
+        screen_geometry = screen.availableGeometry()
+        screen_width = screen_geometry.width()
+        screen_height = screen_geometry.height()
 
+        # 计算全屏的80%大小
+        width = int(screen_width * 0.8)
+        height = int(screen_height * 0.8)
+
+        # 设置窗口大小
+        self.setGeometry(100, 100, width, height)
         # 初始化界面布局
         self.init_ui()
 
