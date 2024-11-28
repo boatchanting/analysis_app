b4cc63f60be80f3b7428c600111688430e142601
boatchanting <162798997+boatchanting@users.noreply.github.com>
2024-11-28 16:49:26 +0800

    Add files via upload

---
+++ method/绘图/折线图（1列）.py
@@ -0,0 +1,21 @@
+# 折线图（1列）.py
+import matplotlib.pyplot as plt
+
+def run(data, column, color="#0000FF", linestyle="-", title="折线图"):
+    """
+    绘制单列数据的折线图
+    :param data: 数据源 (Pandas DataFrame)
+    :param column: 需要绘制的列名
+    :param color: 线的颜色
+    :param linestyle: 线型
+    :param title: 图表标题
+    """
+    plt.rc('font', family='SimHei')  
+    plt.rc('axes', unicode_minus=False)  
+    plt.rcParams['font.sans-serif'] = ['SimHei']  
+    plt.plot(data[column], color=color, linestyle=linestyle)
+    plt.title(title)
+    plt.xlabel("索引")
+    plt.ylabel(column)
+    plt.grid(True)
+    plt.show()
